8a903066be32121c14812a4679b88f0d66f2ebbb
Kracekumar Ramaraju <me@kracekumar.com>
2016-08-25 12:45:51 +0530

    Add initial files

---
+++ book_store.py
@@ -0,0 +1,70 @@
+"""
+1. Write a snippet to get details of the book from user.
+Details are name, author, year, rating and genre.
+"""
+
+"""
+2. Refactor all input code into a function
+"""
+
+"""
+3. Get user input to continue the input for one more movie.
+If the user enters `y` ask details for one more movie and
+for `n` exit the program.
+While exiting the program print all the movie details.
+"""
+
+"""
+4. When user inputs `n`, user has given two movies details.
+Let's store two movies in a list and each movie details in a tuple.
+While exiting the program print all the movie details.
+"""
+
+"""
+5. Convert the program to collect as many movies user wants.
+While exiting the program print all the movie details.
+"""
+
+"""
+6. While exiting the program store the information to a file named
+'book_store.txt'.
+"""
+
+"""
+7. When the program starts read the entered movie details from the text file.
+Print total records in read from the file.
+"""
+
+"""
+8. After reading all the records from the files, create a variable
+`books_by_year` which stores list of books released in a particular year.
+"""
+
+"""
+9. Add support to command line arguments.
+Allowed command line arguments are `new`, `print`, `year`.
+If none of the above arguments are passed program should error out.
+If the input is `new`, ask list of questions for creating new movie.
+If the input is `print`, print all the existing movie details.
+If the input is `year`, print all the movies by year.
+"""
+
+"""
+10. Create a class `Movie` with the attributes `name, author, year, rating and
+genre`.
+"""
+
+"""
+11. Refactor the program to store the movie details as an object rather than
+tuple.
+"""
+
+"""
+12. Add a method `to_csv` in the movie class which will
+return comma separated values.
+"""
+
+"""
+13. While entering new movies detail, check the movie exists are not.
+Update the existing movie.
+"""
---
+++ playground.py
@@ -0,0 +1,83 @@
+print("hello world")
+# I am a variable
+# unicode
+# int
+# Arthimetic operations
+# Subtraction
+# division
+# multiplication
+# raised to 2
+# Multi line string
+
+# Methods
+
+# Get input from user
+# name = input('Say me your name: ')
+# print('How you doing {}?'.format(name))
+
+# Functions -- building block
+
+# Square
+
+# Cube
+
+# delimiter
+
+# Data structure
+# Tuple -- Immutable data structure
+
+# Print first weekday
+
+# print second weekday
+
+# print last weekday
+
+# print all working days
+
+# Modify first weekday to 'Jo'
+
+# weekdays[0] = 'Jo'
+
+# List -- Mutable data structure
+
+# Replace 3rd item
+
+# Add a new item at the end
+
+# Find an element in the list
+
+# for Loop
+
+# While loop
+
+# File handling
+
+# Write list of movies to file
+
+# Read list of movies from the file.
+
+# Read file as context manager
+
+# Standard library - CSV
+
+# Write list of name and profession to csv file
+
+# Read list of names from csv file
+
+# Command line arguments
+
+# Dictionary
+
+# Add a new item
+
+# Print key and value
+#
+
+# Get key and values
+
+# Create a class called Person with attributes `first_name`, `last_name`
+# `age`
+
+# Change attribute value
+
+# Create a method
