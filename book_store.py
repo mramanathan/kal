e43126558a2f6358c0cad9150b1d5f57ba23fdb1
Kracekumar Ramaraju <me@kracekumar.com>
2016-08-25 14:45:32 +0530

    Replace movie with book

--- book_store.py
+++ book_store.py
@@ -8,21 +8,21 @@ Details are name, author, year, rating and genre.
 """
 
 """
-3. Get user input to continue the input for one more movie.
-If the user enters `y` ask details for one more movie and
+3. Get user input to continue the input for one more book.
+If the user enters `y` ask details for one more book and
 for `n` exit the program.
-While exiting the program print all the movie details.
+While exiting the program print all the book details.
 """
 
 """
-4. When user inputs `n`, user has given two movies details.
-Let's store two movies in a list and each movie details in a tuple.
-While exiting the program print all the movie details.
+4. When user inputs `n`, user has given two books details.
+Let's store two books in a list and each book details in a tuple.
+While exiting the program print all the book details.
 """
 
 """
-5. Convert the program to collect as many movies user wants.
-While exiting the program print all the movie details.
+5. Convert the program to collect as many book user wants.
+While exiting the program print all the book details.
 """
 
 """
@@ -31,7 +31,7 @@ While exiting the program print all the movie details.
 """
 
 """
-7. When the program starts read the entered movie details from the text file.
+7. When the program starts read the entered book details from the text file.
 Print total records in read from the file.
 """
 
@@ -44,27 +44,27 @@ Print total records in read from the file.
 9. Add support to command line arguments.
 Allowed command line arguments are `new`, `print`, `year`.
 If none of the above arguments are passed program should error out.
-If the input is `new`, ask list of questions for creating new movie.
-If the input is `print`, print all the existing movie details.
-If the input is `year`, print all the movies by year.
+If the input is `new`, ask list of questions for creating new book.
+If the input is `print`, print all the existing book details.
+If the input is `year`, print all the books by year.
 """
 
 """
-10. Create a class `Movie` with the attributes `name, author, year, rating and
+10. Create a class `Book` with the attributes `name, author, year, and
 genre`.
 """
 
 """
-11. Refactor the program to store the movie details as an object rather than
+11. Refactor the program to store the book details as an object rather than
 tuple.
 """
 
 """
-12. Add a method `to_csv` in the movie class which will
+12. Add a method `to_csv` in the book class which will
 return comma separated values.
 """
 
 """
-13. While entering new movies detail, check the movie exists are not.
-Update the existing movie.
+13. While entering new book detail, check the book exists are not.
+Update the existing book.
 """
