78c7e0e8f8c6c13aa32d6074b22e64fcd840315d
Kracekumar Ramaraju <me@kracekumar.com>
2016-08-30 15:01:36 +0530

    Add worked out code

--- book_store.py
+++ book_store.py
@@ -1,10 +1,15 @@
+import csv
+import sys
 """
 1. Write a snippet to get details of the book from user.
-Details are name, author, year, rating and genre.
+Details are name, author, year, and genre.
+"""
+"""
+2. Refactor all input code into a function called `get_input()` and print the value.
 """
 
 """
-2. Refactor all input code into a function
+2.1 Make `get_input` function to return multiple values.
 """
 
 """
@@ -19,35 +24,78 @@ While exiting the program print all the book details.
 Let's store two books in a list and each book details in a tuple.
 While exiting the program print all the book details.
 """
-
 """
 5. Convert the program to collect as many book user wants.
 While exiting the program print all the book details.
 """
-
 """
 6. While exiting the program store the information to a file named
-'book_store.txt'.
+'book_store.csv'.
 """
 
 """
 7. When the program starts read the entered book details from the text file.
-Print total records in read from the file.
+Print total records read from the file.
 """
-
 """
 8. After reading all the records from the files, create a variable
 `books_by_year` which stores list of books released in a particular year.
+var = {'1918': [('a', '', ''), ()], '2008': [(), ()]}
 """
 
 """
 9. Add support to command line arguments.
-Allowed command line arguments are `new`, `print`, `year`.
+Allowed command line arguments are `new`, `print`
 If none of the above arguments are passed program should error out.
 If the input is `new`, ask list of questions for creating new book.
 If the input is `print`, print all the existing book details.
-If the input is `year`, print all the books by year.
 """
+
+books = []
+
+
+def get_input():
+    print('Welcome to book store')
+    name = input('Book name: ')
+    author = input('Author: ')
+    year = input('Year: ')
+    genre = input('Genre: ')
+    return (name, author, year, genre)
+
+
+def print_book(book):
+    print("Name: {}, Author: {}, Year: {}, Genre: {}".format(
+        book[0], book[1], book[2], book[3]))
+
+
+def main():
+    books = [['The Animal farm', 'George Orwell', '1947', 'Satire'],
+             ['The Outliers', 'Malcom Gladwell', '2008', 'Non-fiction'],
+             ['War and Peace', 'Leo Tolstoy', '1918', 'Novel']]
+    if 'new' in sys.argv:
+        book = get_input()
+        print(book)
+    elif 'print' in sys.argv:
+        print(books)
+    else:
+        print('Argument is invaid')
+
+
+def save_books(books):
+    with open('book_store.csv', 'w') as file:
+        writer = csv.writer(file)
+        for book in books:
+            writer.writerow(book)
+
+
+def read_books():
+    with open('book_store.csv', 'r') as file:
+        reader = csv.reader(file)
+        for line in reader:
+            books.append(line)
+
+read_books()
+main()
 
 """
 10. Create a class `Book` with the attributes `name, author, year, and
--- playground.py
+++ playground.py
@@ -1,37 +1,99 @@
-print("hello world")
+import csv
+# print("hello world")
+# print('Welcome to RV', 23)
 # I am a variable
+name = 'Python'
+# print(name)
+name = 'RV'
+# print(name)
 # unicode
 # int
+name = 'Linux'
+age = 25
+# print(name, age)
+# print('My name is {} and age is {}'.format(name, age))
+# print('My name is %s and age is %d' % (name, age))
 # Arthimetic operations
+a = 2
+b = 3
 # Subtraction
+res = a - b
+# print(res)
+# print(a + b)
+# print(int('23'))
 # division
+a = 4
+b = 2
+res = a / b
+# print(int(res))
+# print(float(1))
 # multiplication
+a = 2
+b = 3.0
+res = a * b
+# print(res)
 # raised to 2
+# print(2 ** 20)
 # Multi line string
+"""this is multi line comment.
+Interpreter doesn't print this line.
+Let's check
+"""
 
+email = """
+Hi {}
+   How are you?
+""".format(name)
+# print(email)
 # Methods
-
+upcase = name.upper()
+# print(upcase)
+# print(name)
+# print(name.lower())
+# print(name.title())
 # Get input from user
 # name = input('Say me your name: ')
 # print('How you doing {}?'.format(name))
-
+# a = input('a:')
+# b = input('b:')
+# print(int(a) + int(b))
+# print('welcome' * 3)
 # Functions -- building block
 
 # Square
 
+def square(no):
+    return no * no
+
+# print(square(2))
+# res = square(no=23)
+# print(res)
 # Cube
 
+# def cube(no=2):
+#     return square(no) * no
+
+# print(cube(8))
+# print(cube())
 # delimiter
+# def delimiter():
+#     print('-' * 80)
+
+# print(delimiter())
 
 # Data structure
 # Tuple -- Immutable data structure
-
+weekdays = ('Monday', 'Tuesday', 'Wednesday', 'Thursday',
+            'Friday', 'Saturday', 'Sunday')
+# print(weekdays)
+# delimiter()
 # Print first weekday
-
+# print(weekdays[0])
 # print second weekday
-
+# print(weekdays[1])
 # print last weekday
-
+# print(weekdays[-1])
+# print(weekdays[-2])
 # print all working days
 
 # Modify first weekday to 'Jo'
@@ -39,37 +101,90 @@ print("hello world")
 # weekdays[0] = 'Jo'
 
 # List -- Mutable data structure
-
+collection = ['Lang', 23, 1989, 34.90]
+# print(collection)
 # Replace 3rd item
-
+collection[2] = 'Python'
+# print(collection)
 # Add a new item at the end
-
+collection.append('Tamil')
+# print(collection)                        
 # Find an element in the list
-
+# print(collection.index(23))
+# print(collection.index('foo'))
+collection.append(weekdays)
+# print(collection)
+# print(collection[-1][-1])
 # for Loop
+# for item in collection:
+#     print(item)
 
 # While loop
+# count = len(collection)
+# print(count)
+# index = 0
+# while index < count:
+#     print(collection[index])
+#     index += 1
 
+# nos = [1, 2, 3, 4]
+# for no in nos:
+#     if no % 2 == 0:
+#         print(no, 'even')
+#     else:
+#         print(no, 'odd')
 # File handling
 
 # Write list of movies to file
-
+file = open('movies.txt', 'w')
+file.write('Fight Club\n')
+file.write('The batman\n')
+file.close()
 # Read list of movies from the file.
 
+# file = open('movies.txt', 'r')
+# for line in file:
+#     print(line)
+# file.close()
 # Read file as context manager
+# with open('movies.txt', 'r') as file:
+#     for line in file:
+#         print(line)
 
 # Standard library - CSV
+with open('movies.csv', 'w') as file:
+    writer = csv.writer(file)
+    writer.writerow(['The Batman', '2010'])
+    writer.writerow(['Fight Club', '2000'])
+
+# delimiter()
+with open('movies.csv', 'r') as file:
+    reader = csv.reader(file)
+    for line in reader:
+        # print(line)
+        pass
 
 # Write list of name and profession to csv file
 
 # Read list of names from csv file
 
 # Command line arguments
+import sys
+print(sys.argv)
+if 'read' in sys.argv:
+    print('you passed read argument', sys.argv[-1])
 
 # Dictionary
-
+rating = {'The God father': 9.8, 'The Batman': 9,
+          'Langs': ['Python'], 'a': 23}
+# print(rating['lang'])
+for key, val in rating.items():
+    if key == 'Langs':
+        rating[key].append('python')
 # Add a new item
-
+rating['The Joker'] = 8.9
+print('The Joker' in rating)
+print(rating)
 # Print key and value
 #
 
@@ -78,6 +193,39 @@ print("hello world")
 # Create a class called Person with attributes `first_name`, `last_name`
 # `age`
 
+class Person:
+    def __init__(self, first_name, year_of_birth,
+                 last_name=''):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.year_of_birth = year_of_birth
+
+    def __repr__(self):
+        return "Person(first_name={}, last_name={}, year_of_birth={})".format(self.first_name, self.last_name, self.year_of_birth)
+
+    def age(self):
+        return 2016 - self.year_of_birth
+
+
+krace = Person('Krace', 1989, 'Kumar')
+siva = Person(last_name='Kumar', first_name='Siva',
+              year_of_birth=1984)
+print(krace.first_name, krace.last_name)
+print(krace)
+print(siva.first_name, siva.last_name)
+print(siva.age())
 # Change attribute value
+siva.first_name = 'Siva Subramaniam'
+print(siva.first_name)
 
 # Create a method
+
+
+
+
+
+
+
+
+
+
