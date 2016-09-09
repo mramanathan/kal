541c9742ebdeb2d7709f1bf294e483dbe551631a
Ramanathan M <rus.cahimb@gmail.com>
2016-09-09 20:52:46 +0530

    cosmetic cleanup

--- playground.py
+++ playground.py
@@ -1,39 +1,52 @@
 import csv
+
 # print("hello world")
 # print('Welcome to GITAM', 10)
+
 # I am a variable
 name = 'Python'
+
 # print(name)
 name = 'GITAM'
+
 # print(name)
 # unicode
 # int
 name = 'Linux'
 age = 25
 # print(name, age)
+
 # print('My name is {} and age is {}'.format(name, age))
 # print('My name is %s and age is %d' % (name, age))
+
 # Arthimetic operations
 a = 2
 b = 3
+
 # Subtraction
 res = a - b
+
 # print(res)
 # print(a + b)
 # print(int('23'))
+
 # division
 a = 4
 b = 2
 res = a / b
+
 # print(int(res))
 # print(float(1))
+
 # multiplication
 a = 2
 b = 3.0
 res = a * b
+
 # print(res)
 # raised to 2
 # print(2 ** 20)
+
 # Multi line string
 """this is multi line comment.
 Interpreter doesn't print this line.
@@ -44,37 +57,41 @@ email = """
 Hi {}
    How are you?
 """.format(name)
+
 # print(email)
 # Methods
 upcase = name.upper()
+
 # print(upcase)
 # print(name)
 # print(name.lower())
 # print(name.title())
+
 # Get input from user
 # name = input('Say me your name: ')
 # print('How you doing {}?'.format(name))
 # a = input('a:')
 # b = input('b:')
 # print(int(a) + int(b))
+
 # print('welcome' * 3)
 # Functions -- building block
 
 # Square
-
 def square(no):
     return no * no
 
 # print(square(2))
 # res = square(no=23)
 # print(res)
-# Cube
 
+# Cube
 # def cube(no=2):
 #     return square(no) * no
 
 # print(cube(8))
 # print(cube())
+
 # delimiter
 # def delimiter():
 #     print('-' * 80)
@@ -85,6 +102,7 @@ def square(no):
 # Tuple -- Immutable data structure
 weekdays = ('Monday', 'Tuesday', 'Wednesday', 'Thursday',
             'Friday', 'Saturday', 'Sunday')
+
 # print(weekdays)
 # delimiter()
 # Print first weekday
@@ -102,19 +120,24 @@ weekdays = ('Monday', 'Tuesday', 'Wednesday', 'Thursday',
 
 # List -- Mutable data structure
 collection = ['Lang', 23, 1989, 34.90]
+
 # print(collection)
 # Replace 3rd item
 collection[2] = 'Python'
+
 # print(collection)
 # Add a new item at the end
 collection.append('Telugu')
+
 # print(collection)                        
 # Find an element in the list
 # print(collection.index(23))
 # print(collection.index('foo'))
 collection.append(weekdays)
+
 # print(collection)
 # print(collection[-1][-1])
+
 # for Loop
 # for item in collection:
 #     print(item)
@@ -140,6 +163,7 @@ file = open('movies.txt', 'w')
 file.write('The Secret World of Pets\n')
 file.write('Big Fat Giant\n')
 file.close()
+
 # Read list of movies from the file.
 
 # file = open('movies.txt', 'r')
@@ -154,8 +178,8 @@ file.close()
 # Standard library - CSV
 with open('movies.csv', 'w') as file:
     writer = csv.writer(file)
-    writer.writerow(['The Batman', '2010'])
-    writer.writerow(['Fight Club', '2000'])
+    writer.writerow(['Roja', '1996'])
+    writer.writerow(['Sagara Sangamam', '1983'])
 
 # delimiter()
 with open('movies.csv', 'r') as file:
@@ -184,6 +208,7 @@ for key, val in rating.items():
 rating['PyCon'] = 2016
 print('PyCon' in rating)
 print(rating)
+
 # Print key and value
 #
 
@@ -209,10 +234,12 @@ class Person:
 ram = Person('Ram', 1973, 'M')
 siva = Person(last_name='Kumar', first_name='Siva',
               year_of_birth=1984)
+
 print(ram.first_name, ram.last_name)
 print(ram)
 print(siva.first_name, siva.last_name)
 print(siva.age())
+
 # Change attribute value
 siva.first_name = 'Siva Subramaniam'
 print(siva.first_name)
